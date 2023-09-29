0f9f2d27b1f48f0495d339baa5520e3b39709496
Abdulkosim <kasim2509@mail.ru>
2023-09-29 23:31:33 +0900

    fixed category

--- product/admin.py
+++ product/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import Product, Rating, Comment
+from .models import Product, Rating, Comment, Category
 
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Rating)
 admin.site.register(Comment)
+admin.site.register(Category)
