4444f110445e4c512a3ce58bf7859ea5c0b3bada
Abdulkosim <kasim2509@mail.ru>
2023-09-29 23:49:40 +0900

    fixed category ser

--- product/serializers.py
+++ product/serializers.py
@@ -27,11 +27,11 @@ class ProductSerializer(serializers.ModelSerializer):
     # likes = LikeSerializer(many=True, read_only=True)
     comments = CommentSerializer(many=True, read_only=True)
 
-    # def to_representation(self, instance):
-    #     representation =  super().to_representation(instance)
-    #     representation['total_likes'] = instance.likes.filter(is_like=True).count()
-    #     representation['profile_image'] = instance.owner.profile_image.url
-    #     return representation
+    def to_representation(self, instance):
+        representation =  super().to_representation(instance)
+        # representation['total_likes'] = instance.likes.filter(is_like=True).count()
+        representation['category'] = instance.category.title
+        return representation
     
     class Meta:
         model = Product
